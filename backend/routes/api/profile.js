458ec2939533bd5d0ad74fdb93fdfbb6ef3713f3
adamwjo <adamwjo@gmail.com>
2019-07-12 13:55:45 -0400

    set up GET profile route

--- backend/routes/api/profile.js
+++ backend/routes/api/profile.js
@@ -10,11 +10,20 @@ const Profile = require('../../models/Profile.js');
 // GET api/v1/profile/me
 // Gets the profile associated with the user
 // Private access
-router.get('/', auth, 
+router.get('/me', auth, 
     
     async (req, res) => {
         try {
-                       
+            
+            //Find the profile associated with the user by using the token. And populate the query with User info as well.
+            const profile = await Profile.findOne({ user: req.user.id }).populate('user', ['first_name', 'last_name', 'email', 'location']);
+
+            if (!profile) {
+                return res.status(400).json({msg: 'Profile cannot be found'})
+            }
+            
+            res.json(profile);
+            
         } catch (error) {
             console.error(error.message);
             res.status(500).send('Server Error');
