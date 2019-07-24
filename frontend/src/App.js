a3ca2b130b35282f20fb96fdc9934a676b0ae40d
adamwjo <adamwjo@gmail.com>
2019-07-24 13:39:57 -0400

    set up redux store

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,10 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+//Redux
+import { Provider } from 'react-redux';
+import store from './store';
+
 //Styles
 import './App.css';
 import './bootstrap.min.css';
@@ -13,19 +17,21 @@ import Register from './components/register.js';
 
 
 const App = () =>
-    <Router>
-      <Fragment>
-        
-        <Navbar/>
-        <Route exact path="/" component={ Landing }/>
-        <section className="container p-5">
-          <Switch>
-            <Route exact path="/register" component={ Register }/>
-            <Route exact path="/login" component={ Login }/>
-          </Switch>
-        </section> 
-        
-      </Fragment>
-    </Router>
+    <Provider store={store}>
+        <Router>
+          <Fragment>
+            
+            <Navbar/>
+            <Route exact path="/" component={ Landing }/>
+            <section className="container p-5">
+              <Switch>
+                <Route exact path="/register" component={ Register }/>
+                <Route exact path="/login" component={ Login }/>
+              </Switch>
+            </section> 
+            
+          </Fragment>
+        </Router>
+    </Provider>
 
 export default App;
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,10 +3,14 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
+// First the initial state must be defined
 const initialState = {};
 
+// initialize middlware within an array
 const middleware =[thunk];
 
+// The store will hold all state at the highest (app) level.
+// Must be initialized with root reducer, initial state, and any middleware.
 const store = createStore(
     rootReducer, 
     initialState, 
